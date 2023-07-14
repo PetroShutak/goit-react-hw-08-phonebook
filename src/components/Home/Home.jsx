cf31262d1fbe9b56fb765490f2f8a19fadcac163
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-14 14:11:05 +0300

    Update Home.jsx
    for example

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,38 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
+
 const Home = () => {
-    return (
-        <div
-        style={{
-            height: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 40,
-            color: '#010101'
-        }}
-        >
-        Phone Book. Final App. Creating...
-        <br/> 
-        <br/>
-        <Button variant="contained">Get started</Button>
-        </div>
-    );
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  const handleButtonClick = () => {
+    if (isLoggedIn) {
+      <Link to="/contacts">...</Link>
+    } else {
+      alert('Спочатку потрібно увійти в систему'); // Показати сповіщення про вхід в систему
     }
+  };
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      Phone Book. Final App. Creating...
+      <br/>
+      <br/>
+      <Button variant="contained" onClick={handleButtonClick}>
+        Get started
+      </Button>
+    </div>
+  );
+};
 
 export default Home;
