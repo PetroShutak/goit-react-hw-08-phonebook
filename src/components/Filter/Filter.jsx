8514747a12f2e2877d1c353e6a3838d1e0e00925
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-14 16:49:59 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setFilter } from 'redux/contacts/filterSlice';
-import { FilterContainer, FilterInput } from './Filter.styled';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 const Filter = () => {
   const dispatch = useDispatch();
@@ -12,15 +14,18 @@ const Filter = () => {
   };
 
   return (
-    <FilterContainer>
-      Find contacts by name:
-      <br />
-      <FilterInput
+    <Box sx={{ marginBottom: '1rem' }}>
+      <Typography variant="body1" gutterBottom>
+        Find contacts by name:
+      </Typography>
+      <TextField
         type="text"
         onChange={handleFilterChange}
         placeholder="search..."
+        fullWidth
+        margin="normal"
       />
-    </FilterContainer>
+    </Box>
   );
 };
 
