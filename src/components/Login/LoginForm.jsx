aad078f291a17d158afbbdcfda01c6ab561467c7
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-14 15:34:15 +0300

    Update LoginForm.jsx

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,59 +1,100 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 const Login = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleEmailChange = event => {
-    setEmail(event.currentTarget.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  const handlePasswordChange = event => {
-    setPassword(event.currentTarget.value);
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    dispatch(logIn({ email, password }));
+    // Валідація форми
+    if (!email || !password) {
+      setError('Please fill in all fields');
+      return;
+    }
 
-    setEmail('');
-    setPassword('');
+    try {
+      await dispatch(logIn({ email, password }));
+
+      // Очищення полів форми після успішного входу
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      setError('Invalid email or password');
+    }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: '5rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Login
+        </Typography>
 
-      <form onSubmit={handleSubmit} autoComplete="on">
-        <label>
-          Email
-          <input
+        {error && (
+          <Box sx={{ marginBottom: '1rem', color: 'red' }}>{error}</Box>
+        )}
+
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          autoComplete="on"
+          sx={{ width: '100%', maxWidth: '400px' }}
+        >
+          <TextField
             type="email"
             name="email"
             value={email}
             onChange={handleEmailChange}
+            label="Email"
             placeholder="Enter email"
+            required
+            fullWidth
+            margin="normal"
           />
-        </label>
 
-        <label>
-          Password
-          <input
+          <TextField
             type="password"
             name="password"
             value={password}
             onChange={handlePasswordChange}
+            label="Password"
             placeholder="Enter password"
+            required
+            fullWidth
+            margin="normal"
           />
-        </label>
 
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          <Button type="submit" variant="contained" fullWidth>
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
