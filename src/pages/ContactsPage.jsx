2eec5dd5f8f44b1c74960085f3ff78c2e75996e3
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-15 11:12:53 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,27 +1,30 @@
+import Layout from 'components/Layout/Layout';
 import ContactsForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 
 const ContactsPage = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-        padding: '1rem',
-      }}
-    >
-      <section>
-        <ContactsForm />
-      </section>
-      <section>
-        <Filter />
-      </section>
-      <section>
-        <ContactList />
-      </section>
-    </div>
+    <Layout hideFooter={true}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          padding: '1rem',
+        }}
+      >
+        <section>
+          <ContactsForm />
+        </section>
+        <section>
+          <Filter />
+        </section>
+        <section>
+          <ContactList />
+        </section>
+      </div>
+    </Layout>
   );
 };
 
