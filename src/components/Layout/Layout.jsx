fdbb1af3ab0448ad5426cdf9ee890e83c2e7943e
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-15 12:23:33 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,14 +3,17 @@ import Footer from '../Footer/Footer';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
+import { useAuth } from 'redux/auth/useAuth';
 
 const Layout = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <div>
       <Header />
       <div>{children}</div>
-      <Footer />
-      <Suspense fallback={<Loader/>}>
+      {!isLoggedIn && <Footer />}
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
     </div>
