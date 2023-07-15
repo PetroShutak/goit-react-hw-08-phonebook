fa37b7a3ae596d003d3dd24578e49eea9a6b73aa
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-15 10:42:34 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,18 @@
 import Header from 'components/Header/Header';
 import Footer from '../Footer/Footer';
-import { Suspense } from 'react';
+import { Suspense, useLocation } from 'react';
 import { Outlet } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  const hideFooter = location.pathname === '/contacts';
+
   return (
     <div>
       <Header />
       <div>{children}</div>
-      <Footer />
+      {!hideFooter && <Footer />}
       <Suspense fallback={<Loader/>}>
         <Outlet />
       </Suspense>
